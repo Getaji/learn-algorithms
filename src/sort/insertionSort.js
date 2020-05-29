9c235e7a7735a06f658e88a13aeb270a3c86aac3
Getaji <t.tanaka.onion@gmail.com>
2020-05-29 18:57:28 +0900

    add: 挿入ソート (Insertion sort)

---
+++ src/sort/insertionSort.js
@@ -0,0 +1,43 @@
+/**
+ * 挿入ソート
+ * ====
+ * 
+ * 簡単なソートアルゴリズム。
+ * ある程度整列している配列に強い。
+ * 最悪計算時間は O(n^2) 。
+ * 
+ * 1. 0番目と1番目の要素を比較し、順番が逆なら入れ替える。
+ * 2. 2番目の要素が1番目までの要素より小さい場合、前の要素を後ろに1つずらす。
+ * 3. 3番目以降も(2)と同じ操作を繰り返す
+ * 
+ * [挿入ソート - Wikipedia](https://ja.wikipedia.org/wiki/%E6%8C%BF%E5%85%A5%E3%82%BD%E3%83%BC%E3%83%88)
+ * 
+ * この関数は引数に渡された配列を変更します。
+ * 
+ * @param {any[]} array
+ * @return {any[]}
+ */
+
+function insertionSort(array) {
+  const len = array.length;
+
+  for (let i = 1; i < len; ++i) {
+    const v = array[i];
+
+    let j = i;
+
+    for (; j > 0; --j) {
+      if (array[j - 1] > v) {
+        array[j] = array[j - 1];
+      } else {
+        break;
+      }
+    }
+
+    array[j] = v;
+  }
+
+  return array;
+}
+
+module.exports = insertionSort;
---
+++ test/sort/insertionSort.test.js
@@ -0,0 +1,27 @@
+const insertionSort = require("../../src/sort/insertionSort");
+
+describe("挿入ソート", () => {
+  it("既に昇順に並んでいる配列は変更がない", () => {
+    expect(insertionSort([0, 1, 2, 3, 4])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+
+  it("空の配列は変更がない", () => {
+    expect(insertionSort([])).toMatchObject([]);
+  });
+
+  it("値が１つだけの配列は変更がない", () => {
+    expect(insertionSort([0])).toMatchObject([0]);
+  });
+  
+  it("サンプル1: [4, 3, 2, 1, 0]", () => {
+    expect(insertionSort([4, 3, 2, 1, 0])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+  
+  it("サンプル2: [3, 1, 2, 0, 4]", () => {
+    expect(insertionSort([3, 1, 2, 0, 4])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+  
+  it("サンプル3: [4, 1, 2, 3, 0]", () => {
+    expect(insertionSort([4, 1, 2, 3, 0])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+})
