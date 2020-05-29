c51e05a4355b18b32ffac1a101da650545e9980b
Getaji <t.tanaka.onion@gmail.com>
2020-05-29 19:54:40 +0900

    add: バブルソート (Bubble sort) / close #1

---
+++ src/sort/bubbleSort.js
@@ -0,0 +1,36 @@
+/**
+ * バブルソート
+ * ====
+ * 
+ * 簡単なソートアルゴリズム。
+ * 並列処理との親和性が高い。
+ * 最悪計算時間は O(n^2) 。
+ * 
+ * 全ての要素に関して、隣接する要素と比較し順序が逆であれば入れ替える。
+ * これを要素数-1回繰り返す。
+ * 
+ * [バブルソート - Wikipedia](https://ja.wikipedia.org/wiki/%E3%83%90%E3%83%96%E3%83%AB%E3%82%BD%E3%83%BC%E3%83%88)
+ * 
+ * この関数は引数に渡された配列を変更します。
+ * 
+ * @param {any[]} array
+ * @return {any[]}
+ */
+
+function bubbleSort(array) {
+  const len = array.length - 1;
+
+  for (let i = 0; i < len; ++i) {
+    for (let j = len; j > i; --j) {
+      if (array[j] < array[j - 1]) {
+        const v = array[j];
+        array[j] = array[j - 1];
+        array[j - 1] = v;
+      }
+    }
+  }
+
+  return array;
+}
+
+module.exports = bubbleSort;
---
+++ test/sort/bubbleSort.test.js
@@ -0,0 +1,27 @@
+const bubbleSort = require("../../src/sort/bubbleSort");
+
+describe("バブルソート", () => {
+  it("既に昇順に並んでいる配列は変更がない", () => {
+    expect(bubbleSort([0, 1, 2, 3, 4])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+
+  it("空の配列は変更がない", () => {
+    expect(bubbleSort([])).toMatchObject([]);
+  });
+
+  it("値が１つだけの配列は変更がない", () => {
+    expect(bubbleSort([0])).toMatchObject([0]);
+  });
+  
+  it("サンプル1: [4, 3, 2, 1, 0]", () => {
+    expect(bubbleSort([4, 3, 2, 1, 0])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+  
+  it("サンプル2: [3, 1, 2, 0, 4]", () => {
+    expect(bubbleSort([3, 1, 2, 0, 4])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+  
+  it("サンプル3: [4, 1, 2, 3, 0]", () => {
+    expect(bubbleSort([4, 1, 2, 3, 0])).toMatchObject([0, 1, 2, 3, 4]);
+  });
+})
