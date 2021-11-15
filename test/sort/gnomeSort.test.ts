67dedec598827b2a90b6c63587886fb4203d4f4d
Getaji <t.tanaka.onion@gmail.com>
2021-11-15 15:07:56 +0900

    add: ノームソート (Gnome sort) を実装 (#7)
    * add: ノームソートを追加
    
    * fix: コムソート11のテストを修正

--- test/sort/gnomeSort.test.ts
+++ test/sort/gnomeSort.test.ts
@@ -1,4 +1,4 @@
-import combSort11 from "../../src/sort/combSort11";
+import gnomeSort from "../../src/sort/gnomeSort";
 
 /** https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
 function shuffle(array: any[]) {
@@ -11,37 +11,37 @@ function shuffle(array: any[]) {
 
 describe("挿入ソート", () => {
   it("既に昇順に並んでいる配列は変更がない", () => {
-    expect(combSort11([0, 1, 2, 3, 4])).toMatchObject([0, 1, 2, 3, 4]);
+    expect(gnomeSort([0, 1, 2, 3, 4])).toMatchObject([0, 1, 2, 3, 4]);
   });
 
   it("空の配列は変更がない", () => {
-    expect(combSort11([])).toMatchObject([]);
+    expect(gnomeSort([])).toMatchObject([]);
   });
 
   it("値が１つだけの配列は変更がない", () => {
-    expect(combSort11([0])).toMatchObject([0]);
+    expect(gnomeSort([0])).toMatchObject([0]);
   });
 
   it("サンプル1: [4, 3, 2, 1, 0]", () => {
-    expect(combSort11([4, 3, 2, 1, 0])).toMatchObject([0, 1, 2, 3, 4]);
+    expect(gnomeSort([4, 3, 2, 1, 0])).toMatchObject([0, 1, 2, 3, 4]);
   });
 
   it("サンプル2: [3, 1, 2, 0, 4]", () => {
-    expect(combSort11([3, 1, 2, 0, 4])).toMatchObject([0, 1, 2, 3, 4]);
+    expect(gnomeSort([3, 1, 2, 0, 4])).toMatchObject([0, 1, 2, 3, 4]);
   });
 
   it("サンプル3: [4, 1, 2, 3, 0]", () => {
-    expect(combSort11([4, 1, 2, 3, 0])).toMatchObject([0, 1, 2, 3, 4]);
+    expect(gnomeSort([4, 1, 2, 3, 0])).toMatchObject([0, 1, 2, 3, 4]);
   });
 
   it("サンプル4: [4, 1, 2, 3, 0, 6, 5]", () => {
-    expect(combSort11([4, 1, 2, 3, 0, 6, 5])).toMatchObject([
+    expect(gnomeSort([4, 1, 2, 3, 0, 6, 5])).toMatchObject([
       0, 1, 2, 3, 4, 5, 6,
     ]);
   });
 
   it("サンプル5: [0...49] shuffle", () => {
     const array = Array(50).fill(0).map((_, i) => i);
-    expect(combSort11(shuffle(array.slice()))).toMatchObject(array);
+    expect(gnomeSort(shuffle(array.slice()))).toMatchObject(array);
   });
 });
