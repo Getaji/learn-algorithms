33c05d8fdaeb8c8954b94b06f1b0561ffa297d37
Getaji <t.tanaka.onion@gmail.com>
2021-11-15 21:37:29 +0900

    add: バイナリサーチ(Binary search)を実装

---
+++ test/search/binarySearch.test.ts
@@ -0,0 +1,22 @@
+import binarySearch from "../../src/search/binarySearch";
+
+describe("二分探索", () => {
+  it("長さ2の配列と含まれる値", () => {
+    expect(binarySearch([0, 1], 0)).toBe(0);
+    expect(binarySearch([0, 1], 1)).toBe(1);
+  });
+  
+  it("長さ2の配列と含まれない値: -1", () => {
+    expect(binarySearch([0, 1], 2)).toBe(-1);
+    expect(binarySearch([1, 2], 0)).toBe(-1);
+  });
+  
+  it("長さ5以上の配列と含まれる値", () => {
+    const arrayLen5 = Array(5).fill(0).map((_, i) => i);
+    expect(binarySearch(arrayLen5, 1)).toBe(arrayLen5.indexOf(1));
+    
+    // 5...14
+    const arrayLen10 = Array(10).fill(0).map((_, i) => i + 5);
+    expect(binarySearch(arrayLen10, 11)).toBe(arrayLen10.indexOf(11));
+  });
+});
