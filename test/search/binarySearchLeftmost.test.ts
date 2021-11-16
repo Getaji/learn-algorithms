5e09ff025685c76fb2933421b78d9d32e453a4bb
Getaji <t.tanaka.onion@gmail.com>
2021-11-16 12:57:13 +0900

    change: binarySearchLeftmostのテストを変更

--- test/search/binarySearchLeftmost.test.ts
+++ test/search/binarySearchLeftmost.test.ts
@@ -1,22 +1,35 @@
 import binarySearchLeftmost from "../../src/search/binarySearchLeftmost";
 
-describe("二分探索", () => {
-  it("長さ2の配列と含まれる値", () => {
+describe("二分探索・左端", () => {
+  it("含まれる値", () => {
     expect(binarySearchLeftmost([0, 1], 0)).toBe(0);
     expect(binarySearchLeftmost([0, 1], 1)).toBe(1);
+    expect(binarySearchLeftmost([1, 2], 1)).toBe(0);
+    expect(binarySearchLeftmost([1, 2], 2)).toBe(1);
+    expect(binarySearchLeftmost([0, 2, 4], 0)).toBe(0);
+    expect(binarySearchLeftmost([0, 2, 4], 2)).toBe(1);
+    expect(binarySearchLeftmost([0, 2, 4], 4)).toBe(2);
+    expect(binarySearchLeftmost([1, 3, 5], 1)).toBe(0);
+    expect(binarySearchLeftmost([1, 3, 5], 3)).toBe(1);
+    expect(binarySearchLeftmost([1, 3, 5], 5)).toBe(2);
   });
   
-  it("長さ2の配列と含まれない値: -1", () => {
+  it("含まれない値", () => {
     expect(binarySearchLeftmost([0, 1], 2)).toBe(-1);
+    expect(binarySearchLeftmost([0, 1], -1)).toBe(-1);
+    expect(binarySearchLeftmost([1, 2], 3)).toBe(-1);
     expect(binarySearchLeftmost([1, 2], 0)).toBe(-1);
+    expect(binarySearchLeftmost([0, 2, 4], 6)).toBe(-1);
+    expect(binarySearchLeftmost([0, 2, 4], -1)).toBe(-1);
+    expect(binarySearchLeftmost([1, 3, 5], 7)).toBe(-1);
+    expect(binarySearchLeftmost([1, 3, 5], 0)).toBe(-1);
   });
-  
-  it("長さ5以上の配列と含まれる値", () => {
-    const arrayLen5 = Array(5).fill(0).map((_, i) => i);
-    expect(binarySearchLeftmost(arrayLen5, 1)).toBe(arrayLen5.indexOf(1));
-    
-    // 5...14
-    const arrayLen10 = Array(10).fill(0).map((_, i) => i + 5);
-    expect(binarySearchLeftmost(arrayLen10, 11)).toBe(arrayLen10.indexOf(11));
+
+  it("重複した値の左端", () => {
+    expect(binarySearchLeftmost([0, 0], 0)).toBe(0);
+    expect(binarySearchLeftmost([1, 1], 1)).toBe(0);
+    expect(binarySearchLeftmost([0, 0, 0], 0)).toBe(0);
+    expect(binarySearchLeftmost([1, 1, 1], 1)).toBe(0);
+    expect(binarySearchLeftmost([0, 0, 1], 0)).toBe(0);
   });
 });
