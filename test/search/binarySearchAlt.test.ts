b45d0a349edd5d41e6b086acee4d97823c847bf4
Getaji <t.tanaka.onion@gmail.com>
2021-11-16 13:00:43 +0900

    change: binarySearchとbinarySearchAltのテストを変更

--- test/search/binarySearchAlt.test.ts
+++ test/search/binarySearchAlt.test.ts
@@ -1,22 +1,27 @@
 import binarySearchAlt from "../../src/search/binarySearchAlt";
 
 describe("二分探索", () => {
-  it("長さ2の配列と含まれる値", () => {
+  it("含まれる値", () => {
     expect(binarySearchAlt([0, 1], 0)).toBe(0);
     expect(binarySearchAlt([0, 1], 1)).toBe(1);
+    expect(binarySearchAlt([1, 2], 1)).toBe(0);
+    expect(binarySearchAlt([1, 2], 2)).toBe(1);
+    expect(binarySearchAlt([0, 2, 4], 0)).toBe(0);
+    expect(binarySearchAlt([0, 2, 4], 2)).toBe(1);
+    expect(binarySearchAlt([0, 2, 4], 4)).toBe(2);
+    expect(binarySearchAlt([1, 3, 5], 1)).toBe(0);
+    expect(binarySearchAlt([1, 3, 5], 3)).toBe(1);
+    expect(binarySearchAlt([1, 3, 5], 5)).toBe(2);
   });
   
-  it("長さ2の配列と含まれない値: -1", () => {
+  it("含まれない値", () => {
     expect(binarySearchAlt([0, 1], 2)).toBe(-1);
+    expect(binarySearchAlt([0, 1], -1)).toBe(-1);
+    expect(binarySearchAlt([1, 2], 3)).toBe(-1);
     expect(binarySearchAlt([1, 2], 0)).toBe(-1);
-  });
-  
-  it("長さ5以上の配列と含まれる値", () => {
-    const arrayLen5 = Array(5).fill(0).map((_, i) => i);
-    expect(binarySearchAlt(arrayLen5, 1)).toBe(arrayLen5.indexOf(1));
-    
-    // 5...14
-    const arrayLen10 = Array(10).fill(0).map((_, i) => i + 5);
-    expect(binarySearchAlt(arrayLen10, 11)).toBe(arrayLen10.indexOf(11));
+    expect(binarySearchAlt([0, 2, 4], 6)).toBe(-1);
+    expect(binarySearchAlt([0, 2, 4], -1)).toBe(-1);
+    expect(binarySearchAlt([1, 3, 5], 7)).toBe(-1);
+    expect(binarySearchAlt([1, 3, 5], 0)).toBe(-1);
   });
 });
